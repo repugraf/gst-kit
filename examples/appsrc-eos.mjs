0480b6b6fa582f4c8089113abecf9be5aea0e317
Serhii Mamedov <bboyvivus@gmail.com>
2025-09-04 20:31:01 +0300

    Remove unnecessary caps value from example

--- examples/appsrc-eos.mjs
+++ examples/appsrc-eos.mjs
@@ -5,10 +5,7 @@ import { randomBytes } from "node:crypto";
 const pipeline = new Pipeline("appsrc name=mysource is-live=true ! videoconvert ! autovideosink");
 const appsrc = pipeline.getElementByName("mysource");
 
-appsrc.setElementProperty(
-  "caps",
-  "video/x-raw,format=RGB,width=320,height=240,bpp=24,depth=24,framerate=0/1"
-);
+appsrc.setElementProperty("caps", "video/x-raw,format=RGB,width=320,height=240,framerate=0/1");
 
 pipeline.play();
 
