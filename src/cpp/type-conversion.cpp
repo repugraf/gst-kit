3cf6c19f5483af4e40b911c88e59fca9e547150a
Maurits Lamers <maurits@weidestraat.nl>
2025-09-20 22:38:39 +0200

    only do when sure that it is a GValueArray

--- src/cpp/type-conversion.cpp
+++ src/cpp/type-conversion.cpp
@@ -298,10 +298,10 @@ namespace TypeConversion {
         // Handle GstStructure (like stats property)
         const GstStructure *structure = GST_STRUCTURE(boxed_value);
         return gst_structure_to_js(env, structure);
-      } else if (boxed_value && g_type_is_a(G_VALUE_TYPE(gvalue), G_TYPE_BOXED)) {
-        // Try casting to GValueArray
+      } else if (boxed_value && G_VALUE_TYPE(gvalue) == G_TYPE_VALUE_ARRAY) {
+        // Only cast to GValueArray if we're certain it's actually a GValueArray
         GValueArray *arr = static_cast<GValueArray *>(boxed_value);
-        if (arr->n_values > 0) { // sanity check
+        if (arr && arr->n_values > 0) { // sanity check with null check
           Napi::Array jsArr = Napi::Array::New(env, arr->n_values);
           for (guint i = 0; i < arr->n_values; i++) {
             jsArr.Set(i, gvalue_to_js(env, &arr->values[i]));
